43a517a4056c9172e00b23266dedcb1b2b527451
singhrahul <singhrahuljnu@gmail.com>
2022-05-26 12:46:47 +0530

    sleep time increased

--- e2eTest.sh
+++ e2eTest.sh
@@ -3,7 +3,7 @@
 # Send some request through zap proxy
 curl -k https://reqbin.com/echo/get/json -H "Accept: application/json" --proxy http://localhost:8090
 
-sleep 2
+sleep 5
 
 # Get Stats from ZAP
 curl -X GET http://localhost:8090/JSON/stats/view/allSitesStats/ \
