767ef8c65ba12e64b1bad8b3b33efe58c8d33e3b
singhrahul <singhrahuljnu@gmail.com>
2022-05-26 12:41:27 +0530

    fix: script fixed

--- e2eTest.sh
+++ e2eTest.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Send some request through zap proxy
-curl -k https://reqbin.com/echo/get/json -H "Accept: application/json" --proxy http://localhost:8080
+curl -k https://reqbin.com/echo/get/json -H "Accept: application/json" --proxy http://localhost:8090
 
 sleep 2
 
 # Get Stats from ZAP
-curl -X GET http://localhost:8080/JSON/stats/view/allSitesStats/ \
+curl -X GET http://localhost:8090/JSON/stats/view/allSitesStats/ \
   -H 'Accept: application/json' \
-  -H 'X-ZAP-API-Key: ivfost7amnh5relkujqat6pe14'
+  -H 'X-ZAP-API-Key: 12345'
