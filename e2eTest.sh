abfc0ce4a34f14f8fa2353367d531c1b82c14216
singhrahul <singhrahuljnu@gmail.com>
2022-05-30 14:13:15 +0530

    add: newman script added

--- e2eTest.sh
+++ e2eTest.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+
+ZAP_PROXY="127.0.0.1:8090"
+
 # Send some request through zap proxy
-curl -k https://reqbin.com/echo/get/json -H "Accept: application/json" --proxy http://localhost:8090
-curl -k https://reqbin.com/echo/get/json -H "Accept: application/json" --proxy http://localhost:8090
-curl -k https://reqbin.com/echo/get/json -H "Accept: application/json" --proxy http://localhost:8090
+curl -k https://reqbin.com/echo/get/json -H "Accept: application/json" --proxy $ZAP_PROXY
+curl -k https://reqbin.com/echo/get/json -H "Accept: application/json" --proxy $ZAP_PROXY
+curl -k https://reqbin.com/echo/get/json -H "Accept: application/json" --proxy $ZAP_PROXY
+
+export HTTPS_PROXY="$ZAP_PROXY"
+newman run TestCollection.postman_collection.json --env-var "HTTPS_PROXY=$HTTPS_PROXY" --insecure
 
 echo "sleep for 10 sec"
 sleep 10
